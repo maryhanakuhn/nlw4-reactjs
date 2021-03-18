5efab363173c750bc28ee64b7fee510a2b8cbf66
maryhanakuhn@gmail.com <maryhanakuhn@gmail.com>
2021-03-18 07:22:58 -0300

    upload project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.githubusercontent.com/u/20826115?s=400&u=080832a49ef94cf85cb953ea3414eaefc11fc6d3&v=4"
+        alt="Maryhana Kuhn"
+      />
+      <div>
+        <strong>Maryhana Kuhn</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1</p>
+      </div>
+    </div>
+  );
+}
